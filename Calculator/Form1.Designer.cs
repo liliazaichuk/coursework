51517e3d525b45f0cbf314684dea195ae2d0c6e0
liliazaichuk <lilizaychuk2005@gmail.com>
2024-01-10 01:40:44 +0200

    add project to repository

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace Calculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            this.comboBoxFigures = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxFigures
+            // 
+            this.comboBoxFigures.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFigures.FormattingEnabled = true;
+            this.comboBoxFigures.Items.AddRange(new object[] {
+            "Квадрат",
+            "Трикутник",
+            "Коло",
+            "Прямокутник",
+            "Овал",
+            "Ромб",
+            "Трапеція",
+            "Правильний многокутник з n кутів"});
+            this.comboBoxFigures.Location = new System.Drawing.Point(12, 47);
+            this.comboBoxFigures.Name = "comboBoxFigures";
+            this.comboBoxFigures.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFigures.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Оберіть фігуру:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 125);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 227);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 16);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputLabel.Location = new System.Drawing.Point(12, 101);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(0, 16);
+            this.inputLabel.TabIndex = 4;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculateButton.Location = new System.Drawing.Point(12, 181);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(148, 23);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Розрахувати площу";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(486, 300);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFigures);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBoxFigures;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxFigures.SelectedIndexChanged += (sender, e) => ShowInputForm();
+            calculateButton.Click += (sender, e) => CalculateArea();
+        }
+
+        private void ShowInputForm()
+        {
+            inputTextBox.Clear();
+            resultLabel.Text = "";
+
+            switch (comboBoxFigures.SelectedItem.ToString())
+            {
+                case "Квадрат":
+                    inputLabel.Text = $"Введiть сторону квадрата:";
+                    break;
+                case "Коло":
+                    inputLabel.Text = $"Введiть радіус кола:";
+                    break;
+
+                case "Трикутник":
+                    inputLabel.Text = "Введiть три сторони трикутника (через пробіл):";
+                    break;
+
+                case "Прямокутник":
+                    inputLabel.Text = $"Введiть дві сторони прямокутника (через пробiл):";
+                    break;
+                case "Овал":
+                    inputLabel.Text = $"Введiть два радiуса овала (через пробiл):";
+                    break;
+
+                case "Ромб":
+                    inputLabel.Text = "Введiть дві діагоналі ромба (через пробiл):";
+                    break;
+
+                case "Трапеція":
+                    inputLabel.Text = "Введiть нижню основу, верхню основу і висоту (через пробiл):";
+                    break;
+                case "Правильний многокутник з n кутів":
+                    inputLabel.Text = "Введiть кількість кутів і довжину сторони (через пробiл):";
+                    break;
+            }
+        }
+
+        private void CalculateArea()
+        {
+
+            double result = 0.0;
+            string[] inputValues = inputTextBox.Text.Split(' ');
+
+            switch (comboBoxFigures.SelectedItem.ToString())
+            {
+                case "Квадрат":
+                    if (double.TryParse(inputValues[0], out double side))
+                        result = side * side;
+                    break;
+
+                case "Трикутник":
+                    if (double.TryParse(inputValues[0], out double sideA) &&
+                        double.TryParse(inputValues[1], out double sideB) &&
+                        double.TryParse(inputValues[2], out double sideC))
+                    {
+                        double p = (sideA + sideB + sideC) / 2;
+                        result = Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+                    }
+                    break;
+
+                case "Коло":
+                    if (double.TryParse(inputValues[0], out double radius))
+                        result = Math.PI * radius * radius;
+                    break;
+
+                case "Прямокутник":
+                    if (double.TryParse(inputValues[0], out double length) &&
+                        double.TryParse(inputValues[1], out double width))
+                        result = length * width;
+                    break;
+
+                case "Овал":
+                    if (double.TryParse(inputValues[0], out double radius1) &&
+                        double.TryParse(inputValues[1], out double radius2))
+                        result = Math.PI * radius1 * radius2;
+                    break;
+
+                case "Ромб":
+                    if (double.TryParse(inputValues[0], out double diagonal1) &&
+                        double.TryParse(inputValues[1], out double diagonal2))
+                        result = 0.5 * diagonal1 * diagonal2;
+                    break;
+
+                case "Трапеція":
+                    if (double.TryParse(inputValues[0], out double base1) &&
+                        double.TryParse(inputValues[1], out double base2) &&
+                        double.TryParse(inputValues[2], out double height))
+                        result = 0.5 * (base1 + base2) * height;
+                    break;
+                case "Правильний многокутник з n кутів":
+                    if (double.TryParse(inputValues[0], out double n) &&
+                        double.TryParse(inputValues[1], out double a))
+                        result = (n * a * a) / (4 * 1 / Math.Tan((180 / n) * Math.PI / 180));
+                    break;
+            }
+
+            if (result == 0)
+            {
+                resultLabel.Text = $"Помилка: некоректно введені дані\n*підказка: не ціле число слід записати через кому";
+            }
+            else { resultLabel.Text = $"Площа: {result:F2}"; }
+        }
+    }
+}
